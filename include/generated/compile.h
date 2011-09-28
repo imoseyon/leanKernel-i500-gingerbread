622df09f291f8e9fe70441d7dfc7479009c5a836
imoseyon <imoseyon@gmail.com>
2011-09-27 23:40:59 -0700

    these files should not be in git

--- include/generated/compile.h
+++
@@ -1,9 +0,0 @@
-/* This file is auto generated, version 7 */
-/*  PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#7 PREEMPT Fri Sep 23 06:38:13 PDT 2011"
-#define LINUX_COMPILE_TIME "06:38:13"
-#define LINUX_COMPILE_BY "imoseyon"
-#define LINUX_COMPILE_HOST "ubuntu"
-#define LINUX_COMPILE_DOMAIN "corp.meebo.com"
-#define LINUX_COMPILER "gcc version 4.4.5 (Ubuntu/Linaro 4.4.4-14ubuntu4) "
--- include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "2.6.35.7-gc0b925c-dirty"
