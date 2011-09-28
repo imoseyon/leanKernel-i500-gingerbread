6562209174ba347c93e60e0a00aaebc1cf22cfbd
imoseyon <imoseyon@gmail.com>
2011-09-27 22:46:20 -0700

    build script fix

--- build_lean.sh
+++ build_lean.sh
@@ -5,9 +5,10 @@ OPT=0
 
 echo "imoseyon_leanKernel_i500_voodoo_$1" > initramfs_root/kernel_version
 
-rm usr/*.o usr/*.lzma
+rm usr/*.o usr/*.lzma usr/*.gz usr/*.cpio
 
-make -j$CPU_JOB_NUM ARCH=arm CROSS_COMPILE=$TOOLCHAIN/$TOOLCHAIN_PREFIX 
+#make -j$CPU_JOB_NUM ARCH=arm CROSS_COMPILE=$TOOLCHAIN/$TOOLCHAIN_PREFIX modules
+make -j$CPU_JOB_NUM ARCH=arm CROSS_COMPILE=$TOOLCHAIN/$TOOLCHAIN_PREFIX
 
 cp arch/arm/boot/zImage ../zip/kernel_update
 cp .config arch/arm/configs/leani500_defconfig
