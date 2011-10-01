6f698d5bf3782f22775fc26ee8bcfbd56cf093d7
imoseyon <imoseyon@gmail.com>
2011-09-30 22:23:13 -0700

    so files for BLN

---
+++ initramfs_root/sbin/imoseyon.sh
@@ -0,0 +1,9 @@
+#!/system/bin/sh
+
+# install bln
+mount -o rw,remount /system /system
+#cat /res/lights.s5pc110.so > /system/lib/hw/lights.s5pc110.so
+#chmod 755 /system/lib/hw/lights.s5pc110.so
+cat /res/lights.aries.so > /system/lib/hw/lights.aries.so
+chmod 644 /system/lib/hw/lights.aries.so
+mount -o ro,remount /system /system
