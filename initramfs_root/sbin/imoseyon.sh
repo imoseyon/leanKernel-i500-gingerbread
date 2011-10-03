ae59975d14ef3d339ff00213f753d619a3acd4dd
imoseyon <imoseyon@gmail.com>
2011-10-02 21:06:55 -0700

    duh need the right liblight too

--- initramfs_root/sbin/imoseyon.sh
+++ initramfs_root/sbin/imoseyon.sh
@@ -2,8 +2,8 @@
 
 # install bln
 mount -o rw,remount /system /system
-#cat /res/lights.s5pc110.so > /system/lib/hw/lights.s5pc110.so
-#chmod 755 /system/lib/hw/lights.s5pc110.so
-cat /res/lights.aries.so > /system/lib/hw/lights.aries.so
-chmod 644 /system/lib/hw/lights.aries.so
+cat /res/lights.s5pc110.so > /system/lib/hw/lights.s5pc110.so
+chmod 755 /system/lib/hw/lights.s5pc110.so
+#cat /res/lights.aries.so > /system/lib/hw/lights.aries.so
+#chmod 644 /system/lib/hw/lights.aries.so
 mount -o ro,remount /system /system
