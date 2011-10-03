a0cea379729e57d8a5320206934642fbc17ab890
imoseyon <imoseyon@gmail.com>
2011-10-03 00:15:48 -0700

    remove the other file

--- initramfs_root/sbin/imoseyon.sh
+++ initramfs_root/sbin/imoseyon.sh
@@ -6,4 +6,5 @@ cat /res/lights.s5pc110.so > /system/lib/hw/lights.s5pc110.so
 chmod 755 /system/lib/hw/lights.s5pc110.so
 #cat /res/lights.aries.so > /system/lib/hw/lights.aries.so
 #chmod 644 /system/lib/hw/lights.aries.so
+rm /system/lib/hw/lights.aries.so
 mount -o ro,remount /system /system
