85953cec97c136b1e0f345a29e2c9216fe471ac4
imoseyon <imoseyon@gmail.com>
2011-10-04 20:22:48 -0700

    build script mod for building modules

--- build_lean.sh
+++ build_lean.sh
@@ -3,16 +3,17 @@ TOOLCHAIN=/usr/bin/
 TOOLCHAIN_PREFIX=arm-linux-gnueabi-
 OPT=0
 
-echo "imoseyon_leanKernel_charge_voodoo_$1" > initramfs_root.voodoo/kernel_version
+#echo "imoseyon_leanKernel_charge_voodoo_$1" > initramfs_root.voodoo/kernel_version
 
-rm usr/*.o usr/*.lzma
+#rm usr/*.o usr/*.lzma
 
-make -j$CPU_JOB_NUM ARCH=arm CROSS_COMPILE=$TOOLCHAIN/$TOOLCHAIN_PREFIX 
+make -j$CPU_JOB_NUM ARCH=arm CROSS_COMPILE=$TOOLCHAIN/$TOOLCHAIN_PREFIX modules
 
-cp arch/arm/boot/zImage ../zip/kernel_update
-cp .config arch/arm/configs/leancharge_defconfig
-cd ../zip
-rm *.zip
-zip -r imoseyon_leanKernel_charge_voodoo_$1.zip *
-rm /tmp/*.zip
-cp *.zip /tmp
+#cp arch/arm/boot/zImage ../zip/kernel_update
+cp .config arch/arm/configs/leani510_defconfig
+#cd ../zip
+#rm *.zip
+#zip -r imoseyon_leanKernel_charge_voodoo_$1.zip *
+#rm /tmp/*.zip
+#cp *.zip /tmp
+find . -name "*.ko" | xargs /usr/bin/arm-linux-gnueabi-strip --strip-unneeded
