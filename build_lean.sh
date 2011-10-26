7dde2881a4ecb1911325a5a2dc09423a52e34dff
imoseyon <imoseyon@gmail.com>
2011-10-25 21:46:04 -0700

    build script changes   Use Linaro, for some reason BLN breaks with CodeSourcery

--- build_lean.sh
+++ build_lean.sh
@@ -1,19 +1,22 @@
 CPU_JOB_NUM=2
-TOOLCHAIN=/usr/bin/
-TOOLCHAIN_PREFIX=arm-linux-gnueabi-
+#TOOLCHAIN=/root/CodeSourcery/Sourcery_G++_Lite/bin/
+#TOOLCHAIN_PREFIX=arm-none-linux-gnueabi-
 OPT=0
 
 echo "imoseyon_leanKernel_i500_voodoo_$1" > initramfs_root/kernel_version
 
-find . -name "*.ko" | xargs $TOOLCHAIN/${TOOLCHAIN_PREFIX}strip --strip-unneeded
+#make -j$CPU_JOB_NUM ARCH=arm CROSS_COMPILE=$TOOLCHAIN/$TOOLCHAIN_PREFIX
+make -j$CPU_JOB_NUM ARCH=arm 
+
+#find . -name "*.ko" | xargs $TOOLCHAIN/${TOOLCHAIN_PREFIX}strip --strip-unneeded
+find . -name "*.ko" | xargs arm-linux-gnueabi-strip --strip-unneeded
 cp drivers/staging/zram/zram.ko initramfs_root/lib/modules
 cp lib/lzo/lzo_compress.ko initramfs_root/lib/modules
 cp lib/lzo/lzo_decompress.ko initramfs_root/lib/modules
 
 rm usr/*.o usr/*.lzma usr/*.gz usr/*.cpio
-
-#make -j$CPU_JOB_NUM ARCH=arm CROSS_COMPILE=$TOOLCHAIN/$TOOLCHAIN_PREFIX modules
-make -j$CPU_JOB_NUM ARCH=arm CROSS_COMPILE=$TOOLCHAIN/$TOOLCHAIN_PREFIX
+#make -j$CPU_JOB_NUM ARCH=arm CROSS_COMPILE=$TOOLCHAIN/$TOOLCHAIN_PREFIX
+make -j$CPU_JOB_NUM ARCH=arm
 
 cp arch/arm/boot/zImage ../zip/kernel_update
 cp .config arch/arm/configs/leani500_defconfig
@@ -22,3 +25,4 @@ rm *.zip
 zip -r imoseyon_leanKernel_i500_voodoo_$1.zip *
 rm /tmp/*.zip
 cp *.zip /tmp
+
