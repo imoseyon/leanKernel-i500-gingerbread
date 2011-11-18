967d12220a6073326b79d8f0143ed732ec56a1d9
imoseyon <imoseyon@gmail.com>
2011-11-17 17:07:10 -0800

    gitignore and missing files

--- include/generated/compile.h
+++
@@ -1,9 +0,0 @@
-/* This file is auto generated, version 181 */
-/*  PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#181 PREEMPT Fri Nov 11 11:14:18 PST 2011"
-#define LINUX_COMPILE_TIME "11:14:18"
-#define LINUX_COMPILE_BY "imoseyon"
-#define LINUX_COMPILE_HOST "ubuntu"
-#define LINUX_COMPILE_DOMAIN "corp.meebo.com"
-#define LINUX_COMPILER "gcc version 4.4.5 (Ubuntu/Linaro 4.4.4-14ubuntu4) "
