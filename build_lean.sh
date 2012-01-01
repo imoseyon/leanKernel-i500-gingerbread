df3b53836cd5a261ad89af86f061013eb8ed1d0d
imoseyon <imoseyon@gmail.com>
2011-12-31 16:53:40 -0800

    simplified build script and fixed makefile

--- build_lean.sh
+++ build_lean.sh
@@ -1,12 +1,8 @@
 CPU_JOB_NUM=2
-#TOOLCHAIN=/root/CodeSourcery/Sourcery_G++_Lite/bin/
-#TOOLCHAIN_PREFIX=arm-none-linux-gnueabi-
-OPT=0
 
 echo "imoseyon_leanKernel_i500_voodoo_$1" > initramfs_root/kernel_version
 
-#make -j$CPU_JOB_NUM ARCH=arm CROSS_COMPILE=$TOOLCHAIN/$TOOLCHAIN_PREFIX
-make -j$CPU_JOB_NUM ARCH=arm 
+make -j$CPU_JOB_NUM
 
 #find . -name "*.ko" | xargs $TOOLCHAIN/${TOOLCHAIN_PREFIX}strip --strip-unneeded
 find . -name "*.ko" | xargs arm-linux-gnueabi-strip --strip-unneeded
@@ -15,8 +11,7 @@ cp lib/lzo/lzo_compress.ko initramfs_root/lib/modules
 cp lib/lzo/lzo_decompress.ko initramfs_root/lib/modules
 
 rm usr/*.o usr/*.lzma usr/*.gz usr/*.cpio
-#make -j$CPU_JOB_NUM ARCH=arm CROSS_COMPILE=$TOOLCHAIN/$TOOLCHAIN_PREFIX
-make -j$CPU_JOB_NUM ARCH=arm
+make -j$CPU_JOB_NUM
 
 cp arch/arm/boot/zImage ../zip/kernel_update
 cp .config arch/arm/configs/leani500_defconfig
